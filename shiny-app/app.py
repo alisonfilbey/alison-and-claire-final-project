9afb11322d826cc62b7794598d41a55afabb59e3
claireconzelmann <cconzelmann@uchicago.edu>
2024-11-21 14:46:21 -0600

    transferring shiny app to shiny dashboard

--- shiny-app/app.py
+++ shiny-app/app.py
@@ -5,6 +5,14 @@ from shapely import wkt
 import numpy as np
 import contextily as ctx
 
+# Load data and fix community column
+roads = gpd.read_file("shiny-app/roads.shp")
+crashes = gpd.read_file("shiny-app/ped_crashes.shp")
+community = gpd.read_file("shiny-app/comm_areas.shp")
+
+roads['community_fixed'] = roads['COMMUNITY'].astype(str).str.strip()
+crashes['community_fixed'] = crashes['COMMUNITY'].astype(str).str.strip()
+community['community_fixed'] = community['COMMUNITY'].astype(str).str.strip()
 
 from shiny import App, reactive, render, ui
 app_ui = ui.page_sidebar(
@@ -13,112 +21,138 @@ app_ui = ui.page_sidebar(
     ),
     ui.layout_column_wrap(
         ui.value_box(
-            "Value", 
-            ui.output_text("top_roads"),
+            ui.output_text("dangerous_roads_label"), 
+            ui.output_table("road_stats", style="font-size: 14px; text-align: center"),
         ),
         fill=False, 
+        style="height: 200px;"
     ),
     ui.layout_columns(
         ui.card(
-            ui.card_header("Road Map of Neighborhood by Number of Pedestrians Severely or Fatally Injured In Crash"),
+            ui.card_header(ui.output_text("community_map_label")),
             ui.output_plot("road_map"),
             full_screen=True,
         ),
         ui.card(
-            ui.card_header("Chicago Neighborhoods by Number of Pedestrians Severely or Fatally Injured In Crash"),
+            ui.card_header("Chicago Neighborhoods by Number of Pedestrians Severely Injured In Crash"),
             ui.output_plot("neighborhood_plot"),
             full_screen=True,
         ),
+        style="height: 600px;"
     ),
-    title="Severe Pedestrian Crashes by Chicago Neighborhood",
+    title="Severe Pedestrian Involved Crashes in Chicago",
     fillable=True,  
 )
 
 
 def server(input, output, session):
+     @reactive.calc
+     def roads_list():
+          df = roads
+          return df
+        
+     @reactive.calc
+     def roads_data():
+          df = roads
+          return df[df['community_fixed'] == input.neighborhood()]
 
-    @reactive.calc
-    def roads_data():
-        df = gpd.read_file("shiny-app/roads.shp")
-        return df[df['COMMUNITY'] == input.neighborhood()]
+     @reactive.calc
+     def community_data():
+          df = community
+          return df[df['community_fixed'] == input.neighborhood()]
 
-    @reactive.calc
-    def crashes_data():
-        df = gpd.read_file("shiny-app/ped_crashes.shp")
-        return df[df['COMMUNITY'] == input.neighborhood()]
+     @reactive.calc
+     def crashes_data():
+          df = crashes
+          return df[df['community_fixed'] == input.neighborhood()]
+          
+     @render.text
+     def dangerous_roads_label():
+         return f"The most dangerous roads for pedestrians in {input.neighborhood()} are:"
 
-    @reactive.calc
-    def community_data():
-        df = gpd.read_file("shiny-app/comm_areas.shp")
-        return df[df['COMMUNITY'] == input.neighborhood()]
+     @render.text
+     def community_map_label():
+         return f"Roadway locations of severe pedestrian crashes in {input.neighborhood()}"
 
-    @reactive.calc
-    def crashes_by_road():
-        road_df = roads_data()
-        crash_df = crashes_data()
+     @reactive.calc
+     def crashes_by_road():
+          road_df = roads_data()
+          crash_df = crashes_data()
 
-        #group by street and calculate number of crashes
-        severe_crashes_by_road = crash_df.groupby("STREET_NAM")[
-            ["n_peds_tot", "n_peds_sev"]].sum().reset_index()
+          #group by street and calculate number of crashes
+          severe_crashes_by_road = crash_df.groupby("STREET_NAM")[
+               ["n_peds_tot", "n_peds_sev"]].sum().reset_index()
 
-        #calculate share of severe ped crashes by speed
-        severe_crashes_by_road["share_severe"] = severe_crashes_by_road[
-            "n_peds_sev"]/severe_crashes_by_road["n_peds_tot"]
-        road_df = pd.merge(road_df, severe_crashes_by_road, on="STREET_NAM", how="left")
-        return road_df
+          #calculate share of severe ped crashes by speed
+          severe_crashes_by_road["share_severe"] = severe_crashes_by_road[
+               "n_peds_sev"]/severe_crashes_by_road["n_peds_tot"]
+          road_df = pd.merge(road_df, severe_crashes_by_road, on="STREET_NAM", how="left")
+          return road_df
 
-    #@reactive.calc
-    #def top_road_data():
-        #df_crash = crashes_data()
-        #df = df_crash.groupby("STREET_NAM")[["n_peds_tot", "n_peds_sev"]].sum().reset_index()
-        #df["share_severe"] = df["n_peds_sev"]/df["n_peds_tot"]
-        #df_crash = pd.merge(df_crash, df, on="STREET_NAM", how="left")
-        #return df_crash
-     
-    @reactive.calc
-    def mode(series):
-        return series.mode().iloc[0] if not series.mode().empty else None
+     @reactive.calc
+     def top_roads():
+          road_df = roads_data()
+          crash_df = crashes_data()
 
-    @render.text
-    def top_roads():
-        crash_df = crashes_data()
-        road_df = crashes_by_road()
-        neighborhood = input.neighborhood()
-        most_frequent_speed = crash_df.groupby("STREET_NAM")["binned_pos"].apply(mode).reset_index()
-        top_roads = road_df.sort_values(by="n_peds_sev", ascending=False)[["STREET_NAM", "n_peds_sev"]].head(3)
-        top_roads = pd.merge(top_roads, most_frequent_speed, on="STREET_NAM", how="left")
-        top_roads = top_roads.rename(columns={
+          #group by street and calculate number of crashes
+          severe_crashes_by_road = crash_df.groupby("STREET_NAM")[
+               ["n_peds_tot", "n_peds_sev"]].sum().reset_index()
+
+          #calculate share of severe ped crashes by speed
+          severe_crashes_by_road["share_severe"] = severe_crashes_by_road[
+               "n_peds_sev"]/severe_crashes_by_road["n_peds_tot"]
+          return severe_crashes_by_road
+
+          
+    
+     def mode(series):
+          return series.mode().iloc[0] if not series.mode().empty else None
+
+
+     @render.table
+     def road_stats():
+          crash_df = crashes_data()
+          road_df = top_roads()
+
+          #calculate speed limit on road by finding most frequent recorded speed
+          most_frequent_speed = crash_df.groupby("STREET_NAM")["binned_pos"].apply(mode).reset_index()
+          
+          #find 3 highest ped crash roads and merge to speed limit
+          road_stats = road_df.sort_values(by="n_peds_sev", ascending=False)[
+               ["STREET_NAM", "n_peds_sev"]].head(3)
+          road_stats = pd.merge(road_stats, most_frequent_speed, on="STREET_NAM", how="left")
+          
+          #rename columns for displaying
+          road_stats = road_stats.rename(columns={
             "STREET_NAM": "Street Name",
-            "n_peds_sev": "Number of Severe Ped Crashes",
-            "binned_pos": "Speed Limit"
-        })
-        print(f"The most dangerous roads in {neighborhood} for pedestrians are:")
-        print(top_roads)
-        return top_roads
+            "n_peds_sev": "Severe Ped Crashes",
+            "binned_pos": "Speed Limit"})
+          return road_stats
 
-    @render.plot
-    def road_map():
-        roads = crashes_by_road()
-        comm_areas = community_data()
+     @render.plot
+     def road_map():
+          roads = crashes_by_road()
+          comm_areas = community_data()
+        
+          #reproject roads and communities so basemap plots correctly
+          comm_areas = comm_areas.to_crs(epsg=3857)
+          roads = roads.to_crs(epsg=3857)
 
-        #reproject so that basemap can plot
-        comm_areas = comm_areas.to_crs(epsg=3857)
-        roads = roads.to_crs(epsg=3857)
+          #plot roads by severe ped crashes
+          fig, ax = plt.subplots(figsize=(5, 5))
+          comm_areas.plot(ax=ax, facecolor="none", edgecolor="black")
+          roads.plot(column="n_peds_sev", legend=True, cmap="RdYlGn_r", ax=ax,
+          legend_kwds={"label": "Number of Severe Pedestrian Crashes", "orientation": "horizontal"})
+          ctx.add_basemap(ax, source=ctx.providers.CartoDB.Positron)
+          ax.set_axis_off()
+          ax.set_title(f"Roadway Locations of Severe Pedestrian Crashes in {input.neighborhood()}", fontsize=14)
+          return fig
 
-        fig, ax = plt.subplots(figsize=(10, 8))
-        comm_areas.plot(ax=ax, facecolor="none", edgecolor="black")
-        roads.plot(column="n_peds_sev", legend=True, cmap="RdYlGn_r", ax=ax,
-                  legend_kwds={"label": "Number of Severe Pedestrian Crashes", "orientation": "horizontal"})
-        ctx.add_basemap(ax, source=ctx.providers.CartoDB.Positron)
-        ax.set_axis_off()
-        ax.set_title("Roadway Locations of Severe Pedestrian Crashes in Hyde Park", fontsize=14)
-        return fig
-
-    @reactive.effect
-    def _():
-        neighborhood_list = community_data()['COMMUNITY'].unique().tolist()
-        neighborhood_list = sorted(neighborhood_list)
-        ui.update_select("neighborhood", choices=neighborhood_list)
+     @reactive.effect
+     def _():
+          neighborhood_list = roads_list()["community_fixed"].dropna().unique().tolist()
+          neighborhood_list = sorted(neighborhood_list)
+          ui.update_select("neighborhood", choices=neighborhood_list)
     
 
 app = App(app_ui, server)
--- shiny-app/app_2.py
+++ shiny-app/app_2.py
@@ -9,108 +9,125 @@ from shiny import App, reactive, render, ui
 
 # Load data and check for errors
 try:
-    roads = gpd.read_file("c:/Users/aliso/OneDrive/Documents/Second Year First Quarter/Python II/alison-and-claire-final-project/shiny-app/roads.shp")
-    crashes = gpd.read_file("c:/Users/aliso/OneDrive/Documents/Second Year First Quarter/Python II/alison-and-claire-final-project/shiny-app/ped_crashes.shp")
-    community = gpd.read_file("c:/Users/aliso/OneDrive/Documents/Second Year First Quarter/Python II/alison-and-claire-final-project/shiny-app/comm_areas.shp")
+    roads = gpd.read_file("shiny-app/roads.shp")
+    crashes = gpd.read_file("shiny-app/ped_crashes.shp")
+    community = gpd.read_file("shiny-app/comm_areas.shp")
     print("Data loaded successfully")
 except Exception as e:
     print(f"Error loading shapefiles: {e}")
 
-roads['neighborhood'] = roads['COMMUNITY'].astype(str).str.strip()
-crashes['neighborhood'] = crashes['COMMUNITY'].astype(str).str.strip()
-community['neighborhood'] = community['COMMUNITY'].astype(str).str.strip()
+roads['community_fixed'] = roads['COMMUNITY'].astype(str).str.strip()
+crashes['community_fixed'] = crashes['COMMUNITY'].astype(str).str.strip()
+community['community_fixed'] = community['COMMUNITY'].astype(str).str.strip()
 
 
 app_ui = ui.page_fluid(
     ui.input_select(id="neighborhood", label='Choose a neighborhood:', choices=[]),
     #ui.panel(ui.output_text("top_roads")),
-    ui.output_table("roads_data_table"),  # Display roads data
+    ui.output_text("road_stats"),  
     ui.output_plot("road_map")
 )
 
 def server(input, output, session):
-   @reactive.calc
-   def roads_list():
-        df = roads
-        return df
-   def roads_data():
-        df = roads
-        return df[df['neighborhood'] == input.neighborhood()]
-   @reactive.calc
-   def community_data():
-        df = community
-        return df[df['neighborhood'] == input.neighborhood()]
-   @reactive.calc
-   def crashes_data():
-        df = crashes
-        return df[df['neighborhood'] == input.neighborhood()]
-   @render.table
-   def  roads_data_table():
-        df = roads_data()  # Get the roads data
-        return df.head() 
+     @reactive.calc
+     def roads_list():
+          df = roads
+          return df
+        
+     @reactive.calc
+     def roads_data():
+          df = roads
+          return df[df['community_fixed'] == input.neighborhood()]
 
-   @reactive.calc
-   def crashes_by_road():
-        road_df = roads_data()
-        crash_df = crashes_data()
-        #group by street and calculate number of crashes
-        severe_crashes_by_road = crash_df.groupby("STREET_NAM")[
-            ["n_peds_tot", "n_peds_sev"]].sum().reset_index()
-        #calculate share of severe ped crashes by speed
-        severe_crashes_by_road["share_severe"] = severe_crashes_by_road[
-            "n_peds_sev"]/severe_crashes_by_road["n_peds_tot"]
-        road_df = pd.merge(road_df, severe_crashes_by_road, on="STREET_NAM", how="left")
-        return road_df
+     @reactive.calc
+     def community_data():
+          df = community
+          return df[df['community_fixed'] == input.neighborhood()]
 
-    #@reactive.calc
-    #def top_road_data():
-      #  df_crash = crashes_data()
-      #  df = df_crash.groupby("STREET_NAM")[["n_peds_tot", "n_peds_sev"]].sum().reset_index()
-      #  df["share_severe"] = df["n_peds_sev"]/df["n_peds_tot"]
-      #  df_crash = pd.merge(df_crash, df, on="STREET_NAM", how="left")
-      #  return df_crash
-     
-     #@reactive.calc
-     #def mode(series):
-        #return series.mode().iloc[0] if not series.mode().empty else None
+     @reactive.calc
+     def crashes_data():
+          df = crashes
+          return df[df['community_fixed'] == input.neighborhood()]
 
-     #@render.text
-     #def top_roads():
-       # crash_df = crashes_data()
-      #  road_df = crashes_by_road()
-      #  neighborhood = input.neighborhood()
-      #  most_frequent_speed = crash_df.groupby("STREET_NAM")["binned_pos"].apply(mode).reset_index()
-      #  top_roads = road_df.sort_values(by="n_peds_sev", ascending=False)[["STREET_NAM", "n_peds_sev"]].head(3)
-      #  top_roads = pd.merge(top_roads, most_frequent_speed, on="STREET_NAM", how="left")
-      #  top_roads = top_roads.rename(columns={
-      #      "STREET_NAM": "Street Name",
-       #     "n_peds_sev": "Number of Severe Ped Crashes",
-       #     "binned_pos": "Speed Limit"
-       #  })
-      #  print(f"The most dangerous roads in {neighborhood} for pedestrians are:")
-     #   print(top_roads)
-      #  return top_roads
+     @reactive.calc
+     def crashes_by_road():
+          road_df = roads_data()
+          crash_df = crashes_data()
 
-   @render.plot
-   def road_map():
-        roads = crashes_by_road()
-        comm_areas = community_data()
-        comm_areas = comm_areas.to_crs(epsg=3857)
-        roads = roads.to_crs(epsg=3857)
-        fig, ax = plt.subplots(figsize=(5, 5))
-        comm_areas.plot(ax=ax, facecolor="none", edgecolor="black")
-        roads.plot(column="n_peds_sev", legend=True, cmap="RdYlGn_r", ax=ax,
-        legend_kwds={"label": "Number of Severe Pedestrian Crashes", "orientation": "horizontal"})
-        ctx.add_basemap(ax, source=ctx.providers.CartoDB.Positron)
-        ax.set_axis_off()
-        ax.set_title("Roadway Locations of Severe Pedestrian Crashes in Neighborhood", fontsize=14)
-        return fig
+          #group by street and calculate number of crashes
+          severe_crashes_by_road = crash_df.groupby("STREET_NAM")[
+               ["n_peds_tot", "n_peds_sev"]].sum().reset_index()
 
-   @reactive.effect
-   def _():
-       neighborhood_list = roads_list()['neighborhood'].dropna().unique().tolist()
-       neighborhood_list = sorted(neighborhood_list)
-       ui.update_select("neighborhood", choices=neighborhood_list)
+          #calculate share of severe ped crashes by speed
+          severe_crashes_by_road["share_severe"] = severe_crashes_by_road[
+               "n_peds_sev"]/severe_crashes_by_road["n_peds_tot"]
+          road_df = pd.merge(road_df, severe_crashes_by_road, on="STREET_NAM", how="left")
+          return road_df
+
+     @reactive.calc
+     def top_roads():
+          road_df = roads_data()
+          crash_df = crashes_data()
+
+          #group by street and calculate number of crashes
+          severe_crashes_by_road = crash_df.groupby("STREET_NAM")[
+               ["n_peds_tot", "n_peds_sev"]].sum().reset_index()
+
+          #calculate share of severe ped crashes by speed
+          severe_crashes_by_road["share_severe"] = severe_crashes_by_road[
+               "n_peds_sev"]/severe_crashes_by_road["n_peds_tot"]
+          return severe_crashes_by_road
+
+          
+    
+     def mode(series):
+          return series.mode().iloc[0] if not series.mode().empty else None
+
+
+     @render.text
+     def road_stats():
+          crash_df = crashes_data()
+          road_df = top_roads()
+
+          #calculate speed limit on road by finding most frequent recorded speed
+          most_frequent_speed = crash_df.groupby("STREET_NAM")["binned_pos"].apply(mode).reset_index()
+          
+          #find 3 highest ped crash roads and merge to speed limit
+          road_stats = road_df.sort_values(by="n_peds_sev", ascending=False)[
+               ["STREET_NAM", "n_peds_sev"]].head(3)
+          road_stats = pd.merge(road_stats, most_frequent_speed, on="STREET_NAM", how="left")
+          
+          #rename columns for displaying
+          road_stats = road_stats.rename(columns={
+            "STREET_NAM": "Street Name",
+            "n_peds_sev": "Number of Severe Ped Crashes",
+            "binned_pos": "Speed Limit"})
+          return road_stats
+
+     @render.plot
+     def road_map():
+          roads = crashes_by_road()
+          comm_areas = community_data()
+        
+          #reproject roads and communities so basemap plots correctly
+          comm_areas = comm_areas.to_crs(epsg=3857)
+          roads = roads.to_crs(epsg=3857)
+
+          #plot roads by severe ped crashes
+          fig, ax = plt.subplots(figsize=(5, 5))
+          comm_areas.plot(ax=ax, facecolor="none", edgecolor="black")
+          roads.plot(column="n_peds_sev", legend=True, cmap="RdYlGn_r", ax=ax,
+          legend_kwds={"label": "Number of Severe Pedestrian Crashes", "orientation": "horizontal"})
+          ctx.add_basemap(ax, source=ctx.providers.CartoDB.Positron)
+          ax.set_axis_off()
+          ax.set_title(f"Roadway Locations of Severe Pedestrian Crashes in {input.neighborhood()}", fontsize=14)
+          return fig
+
+     @reactive.effect
+     def _():
+          neighborhood_list = roads_list()["community_fixed"].dropna().unique().tolist()
+          neighborhood_list = sorted(neighborhood_list)
+          ui.update_select("neighborhood", choices=neighborhood_list)
 
 app = App(app_ui, server)
 
